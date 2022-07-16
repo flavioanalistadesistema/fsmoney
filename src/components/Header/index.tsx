aa971d14b2ad448f9b03ec98955d24013bac77cb
Flávio Santos <flavioanalistadesistema@gmail.com>
2022-07-16 17:06:14 -0300

    feat: Configurando modal de ciriação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import logol from '../../assets/Logo.svg'
 import { Container, Content } from './styles'
 
-export function Header() {
+interface HeaderProps {
+    onOpenNewtransactionModal: () => void
+}
+
+export function Header({ onOpenNewtransactionModal }: HeaderProps) {
+
     return (
         <Container>
             <Content>
                 <img src={logol} alt="fs money" />
-                <button type="button">
+                <button type="button" onClick={onOpenNewtransactionModal}>
                     nova transalçao
                 </button>
             </Content>
